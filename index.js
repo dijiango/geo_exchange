0931de3f2602e35780d69d4f5f4a885b777e97a8
Diana Jiang <jiang99diana@gmail.com>
2021-11-16 10:21:01 -0500

    left/right rotating earth

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
-BASE_URL = "https://api.exchangerate.host/latest";
+const globe = document.querySelector('#current-globe');
+const leftBtn = document.getElementById('left-turn');
+const rightBtn = document.getElementById('right-turn');
+const goHome = document.getElementById('return-home');
 
-fetch(BASE_URL)
-.then(r => r.json())
-.then(data => console.log("This is what we get back", data));
+//image src tag
+let imgNum = 1;
+const baseImg = "earth-images/" + imgNum + ".jpg";
+globe.src = baseImg; 
+
+function rotateLeft() {
+    if (imgNum === 1) {
+        imgNum = 15;
+    }
+    else {
+        --imgNum;
+    }
+    globe.src = "earth-images/" + imgNum + ".jpg";
+    console.log("New image ->", globe.src);
+};
+
+function rotateRight() {
+    if (imgNum === 15) {
+        imgNum = 1;
+        
+    }
+    else {
+        ++imgNum;
+    }
+    globe.src = "earth-images/" + imgNum + ".jpg";
+}
+
+leftBtn.addEventListener("click", rotateLeft);
+
+rightBtn.addEventListener("click", rotateRight);
+
+goHome.addEventListener("click", () => {
+    imgNum = 1;
+    globe.src = "earth-images/" + imgNum + ".jpg";
+});
