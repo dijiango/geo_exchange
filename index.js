4aac30f2387f12ee294aab88065fbb384126c19a
Diana Jiang <jiang99diana@gmail.com>
2021-11-16 18:54:47 -0500

    added all pins

--- index.js
+++ index.js
@@ -44,63 +44,75 @@ goHome.addEventListener("click", () => {
 
 //add pins to images
 function countryPins() {
+    const nullMessage = "Stay tuned for more countries coming to this region!";
+
     if (imgNum === 1) {
         currentCountry.textContent = "You are currently in the US and Canada";
         console.log("Img ", imgNum);
         canadaPin();
+        unitedStatesPin();
     } else if (imgNum === 2) {
         currentCountry.textContent = "You are currently in Brazil";
         console.log("Img ", imgNum);
+        brasilPin();
     } else if (imgNum === 3) {
-        currentCountry.textContent = "You are currently NOT in the US";
+        currentCountry.textContent = nullMessage;
         console.log("Img ", imgNum);
     } else if (imgNum === 4) {
-        currentCountry.textContent = "You are currently NOT in the US";
+        currentCountry.textContent = nullMessage;
         console.log("Img ", imgNum);
     } else if (imgNum === 5) {
         currentCountry.textContent = "You are currently in the UK and Sweden";
         console.log("Img ", imgNum);
+        unitedKingdomPin();
+        swedenPin();
     } else if (imgNum === 6) {
         currentCountry.textContent = "You are currently in Europe and South Africa";
         console.log("Img ", imgNum);
+        europePin();
+        southAfricaPin();
     } else if (imgNum === 7) {
-        currentCountry.textContent = "You are currently NOT in the US";
+        currentCountry.textContent = nullMessage;
         console.log("Img ", imgNum);
     } else if (imgNum === 8) {
         currentCountry.textContent = "You are currently in Russia";
         console.log("Img ", imgNum);
+        russiaPin();
     } else if (imgNum === 9) {
         currentCountry.textContent = "You are currently in Japan and Hong Kong";
         console.log("Img ", imgNum);
+        japanPin();
+        hongKongPin();
     } else if (imgNum === 10) {
         currentCountry.textContent = "You are currently in Australia";
         console.log("Img ", imgNum);
+        aussiePin();
     } else if (imgNum === 11) {
-        currentCountry.textContent = "You are currently NOT in the US";
+        currentCountry.textContent = nullMessage;
         console.log("Img ", imgNum);
     } else if (imgNum === 12) {
-        currentCountry.textContent = "You are currently NOT in the US";
+        currentCountry.textContent = nullMessage;
         console.log("Img ", imgNum);
     } else if (imgNum === 13) {
-        currentCountry.textContent = "You are currently NOT in the US";
+        currentCountry.textContent = nullMessage;
         console.log("Img ", imgNum);
     } else if (imgNum === 14) {
-        currentCountry.textContent = "You are currently NOT in the US";
+        currentCountry.textContent = nullMessage;
         console.log("Img ", imgNum);
     } else if (imgNum === 15) {
-        currentCountry.textContent = "You are currently NOT in the US";
+        currentCountry.textContent = nullMessage;
         console.log("Img ", imgNum);
     }
 };
 
 //creation of button function
-function createPin() {
+function createPin(x) {
     while (currentCountry.hasChildNodes === true) {
         currentCountry.removeChildren();
     }
     const createH1 = document.createElement('h1');
     const pin = document.createElement('img');
-        pin.id = `pin-${imgNum}`;
+        pin.id = `pin-${x}`;
         pin.src = "other-images/pin.png";
         pin.alt = "Pin";
 
@@ -111,6 +123,66 @@ function createPin() {
 //pins for each country
 function canadaPin() {
     if (imgNum === 1) {
-        createPin();
+        createPin('canada');
+    }
+}
+
+function unitedStatesPin() {
+    if (imgNum === 1) {
+        createPin('united-states');
+    }
+}
+
+function brasilPin() {
+    if (imgNum === 2) {
+        createPin('brasil');
+    }
+}
+
+function unitedKingdomPin() {
+    if (imgNum === 5) {
+        createPin('united-kingdom');
+    }
+}
+
+function swedenPin() {
+    if (imgNum === 5) {
+        createPin('sweden');
+    }
+}
+
+function europePin() {
+    if (imgNum === 6) {
+        createPin('europe');
+    }
+}
+
+function southAfricaPin() {
+    if (imgNum === 6) {
+        createPin('south-africa');
+    }
+}
+
+function russiaPin() {
+    if (imgNum === 8) {
+        createPin('russia');
+    }
+}
+
+function japanPin() {
+    if (imgNum === 9) {
+        createPin('japan');
+    }
+}
+
+function hongKongPin() {
+    if (imgNum === 9) {
+        createPin('hong-kong');
+    }
+}
+
+function aussiePin() {
+    if (imgNum === 10) {
+        createPin('aussie');
     }
 }
