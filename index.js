d3b2099a534b04a8228ce96fb08d66168ec22da4
Diana Jiang <jiang99diana@gmail.com>
2021-11-17 16:46:31 -0500

    nav bar

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 //STEPHEN
 const returnCurrencyTrigger = document.getElementById('return-currency');
 const currencyTrigger = document.getElementById('base-currency');
@@ -42,9 +44,9 @@ submitCurrencyForm.addEventListener("submit", (e) => {
         }
     }
     fetch(`https://api.vatcomply.com/rates?${baseCurrency}`)
-.then(response => response.json())
- .then(data => renderData(data))
-})
+    .then(response => response.json())
+     .then(data => renderData(data))
+    })
 
 
 
@@ -87,10 +89,11 @@ const goHome = document.getElementById('return-home');
 const currentCountry = document.querySelector('#country-pins');
 
 //image src tag
-let imgNum = 2;
+let imgNum = 1;
 const baseImg = "earth-images/" + imgNum + ".png";
 globe.src = baseImg; 
 
+//rotate the globe functions
 function rotateLeft() {
     if (imgNum === 1) {
         imgNum = 15;  
@@ -266,3 +269,33 @@ function aussiePin() {
         createPin('aussie');
     }
 }
+
+const newNavbar = document.createElement("nav");
+const geoLocator = document.createElement("a");
+const meetTheTeam = document.createElement("a");
+const aboutTheProject = document.createElement("a");
+const moreFeaturesToCome = document.createElement("a");
+
+newNavbar.className = "navbar";
+geoLocator.href = "./index.html";
+geoLocator.innerText = "Geo Locator";
+meetTheTeam.href = "https://www.cia.gov/the-world-factbook/countries/";
+meetTheTeam.innerText = "Meet the team";
+meetTheTeam.target = "_blank";
+aboutTheProject.href = "https://www.federalreserve.gov/releases/h10/current/";
+aboutTheProject.innerText = "About this project";
+aboutTheProject.target = "_blank";
+moreFeaturesToCome.href = "https://www.babypips.com/learn";
+moreFeaturesToCome.innerText = "More features to come";
+moreFeaturesToCome.target = "_blank";
+
+if (document.title === "Geo Exchange | Home") {
+        newNavbar.append(aboutTheProject)
+}   else {
+        newNavbar.append(geoLocator)
+};
+
+document.body.querySelector(".container").prepend(newNavbar);
+    newNavbar.append(meetTheTeam);
+    newNavbar.append(aboutTheProject);
+    newNavbar.append(moreFeaturesToCome);
